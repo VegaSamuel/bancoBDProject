5c99a12022b7132b9e19c1a43a76a80c877e3ff9
Samuel Vega <atrap.rap00z0@gmail.com>
2023-02-15 21:00:26 -0700

    Agregué los métodos a ClientesDAO y quite el método Main en ClientesForm

--- bancoDBProject/src/implementaciones/ClientesDAO.java
+++ bancoDBProject/src/implementaciones/ClientesDAO.java
@@ -1,10 +1,43 @@
 //ClientesDAO.java
 package implementaciones;
 
+import dominio.Cliente;
+import excepciones.DAOException;
+import interfaces.IClientesDAO;
+import interfaces.IConexionBD;
+import java.util.List;
+import java.util.logging.Logger;
+import util.ConfigPaginado;
+
 /**
- *
+ * Esta clase contiene la gestión de persistencia con los datos de los clientes
  * @author Samuel & Victor Gonzales
  */
-public class ClientesDAO {
+public class ClientesDAO implements IClientesDAO {
+    private static final Logger LOG = Logger.getLogger(ClientesDAO.class.getName());
+    private final IConexionBD MANEJADOR_CONEXIONES;
     
+    public ClientesDAO(IConexionBD manejadorConexiones) {
+        this.MANEJADOR_CONEXIONES = manejadorConexiones;
+    }
+    
+    @Override
+    public Cliente consultar(Integer id) throws DAOException {
+        return null;
+    }
+    
+    @Override
+    public Cliente insertar(Cliente cliente) throws DAOException {
+        return null;
+    }
+    
+    @Override
+    public Cliente eliminar(Integer id) throws DAOException {
+        return null;
+    }
+    
+    @Override
+    public List<Cliente> consultar(ConfigPaginado configPaginado) throws DAOException {
+        return null;
+    }
 }
