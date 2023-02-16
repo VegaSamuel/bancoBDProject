8c62eb0017ed31c611aad5cb2893566b2bc1f807
VegaSamuel <atrap.rap00z0@gmail.com>
2023-02-16 14:08:22 -0700

    No mucho

--- bancoDBProject/src/main/java/implementaciones/CuentasDAO.java
+++ bancoDBProject/src/main/java/implementaciones/CuentasDAO.java
@@ -4,21 +4,29 @@ package implementaciones;
 import java.sql.ResultSet;
 import java.util.List;
 import java.util.logging.Logger;
-
 import dominio.Cuenta;
 import excepciones.DAOException;
 import interfaces.IConexionBD;
 import interfaces.ICuentaDAO;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
 import util.ConfigPaginado;
 
 /**
  *
  * @author Samuel Vega & Victor Gonzalez
  */
-public class CuentaDAO implements ICuentaDAO {
-    private static final Logger log = Logger.getLogger(ClientesDAO.class.getName());
-    private final IConexionBD Manager;
+public class CuentasDAO implements ICuentaDAO {
+    private static final Logger LOG = Logger.getLogger(ClientesDAO.class.getName());
+    private final IConexionBD MANAGER;
 
+    public CuentasDAO(IConexionBD manejadorConexiones) {
+        this.MANAGER = manejadorConexiones;
+    }
+    
     @Override
     public Cuenta consultar(Integer numero) throws DAOException {
         try(
@@ -28,20 +36,18 @@ public class CuentaDAO implements ICuentaDAO {
         ){
             comando.setInt(1, numero);
             ResultSet resultado = comando.executeQuery();
-            Cliente cliente = null;
+            Cuenta cuenta = null;
             if (resultado.next()){
                 Integer no_cuenta = resultado.getInt("no_cuenta");
                 Date fecha_apertura = resultado.getDate("fecha_apertura");
                 Float saldo = resultado.getFloat("saldo");
                 Integer id_cliente = resultado.getInt("id_cliente");
-                Cuenta cuenta = new Cuenta(no_cuenta, fecha_apertura, saldo, id_cliente);
+                cuenta = new Cuenta(no_cuenta, fecha_apertura, saldo, id_cliente);
             }
             return cuenta;
         } catch (SQLException ex) {
-            LOG.log(Level.SEVERE, "No se pudo consultar la cuenta "
-                    + "{0}", ex.getMessage());
-            throw new DAOException("No se pudo consultar la "
-                    + "cuenta " + ex.getMessage());
+            LOG.log(Level.SEVERE, "No se pudo consultar la cuenta {0}", ex.getMessage());
+            throw new DAOException("No se pudo consultar la cuenta " + ex.getMessage());
         }
     }
 
