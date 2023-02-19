6eaf5740c2c57ec9ff2768b91b94716892ea4793
Samuel Vega <atrap.rap00z0@gmail.com>
2023-02-18 20:07:56 -0700

    Agregué el form para Retiros

--- bancoDBProject/src/main/java/presentación/CuentaForm.java
+++ bancoDBProject/src/main/java/presentación/CuentaForm.java
@@ -17,7 +17,7 @@ import util.ConfigPaginado;
 
 /**
  *
- * @author Samuel Vega & Victor Gonzalez
+ * @author Samuel Vega & Victor Gonzales
  */
 public class CuentaForm extends javax.swing.JFrame {
     private static final Logger LOG = Logger.getLogger(CuentaForm.class.getName());
--- bancoDBProject/src/main/java/presentación/DomicilioForm.java
+++ bancoDBProject/src/main/java/presentación/DomicilioForm.java
@@ -16,7 +16,7 @@ import util.ConfigPaginado;
 
 /**
  *
- * @author Samuel Vega & Victor Gonzalez
+ * @author Samuel Vega & Victor Gonzales
  */
 public class DomicilioForm extends javax.swing.JFrame {
     private static final Logger LOG = Logger.getLogger(DomicilioForm.class.getName());
